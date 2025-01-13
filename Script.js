1c038486676f0258f42470fa19c00c3be8b139c2
Bismillahfam <ahsanmaleek1.27@gmail.com>
2025-01-13 22:53:02 +0000

    Added an api call
    This stores the prayer times in different variables

--- Script.js
+++ Script.js
@@ -1,4 +1,21 @@
-console.log("hello1")
-fetch("http://api.aladhan.com/v1/timingsByCity/09-01-2024")
-    .then(res => console.log(res))
-console.log("hello")
+async function GetNamazData() {
+    let currentDate = new Date().toLocaleDateString();
+    currentDate = currentDate.replace("/", "-")
+    currentDate = currentDate.replace("/", "-")
+    console.log(currentDate)
+
+    const APIurl = `https://api.aladhan.com/v1/timingsByCity/${currentDate}?city=Glasgow&country=GB`
+    const APIresponse = await (await fetch(APIurl)).json();
+    console.log(APIresponse)
+
+    const { data, ...rest } = APIresponse
+    console.log(data)
+
+    const { timings, ...rest2 } = data
+    console.log(timings)
+
+    let { Fajr, Dhuhr, Asr, Maghrib, Isha } = timings
+    console.log(Fajr, Dhuhr, Asr, Maghrib, Isha)
+}
+
+GetNamazData()
