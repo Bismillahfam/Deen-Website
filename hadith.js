bf12d5422ffb8908b61c90f464507cb1b96f4d28
Bismillahfam <ahsanmaleek1.27@gmail.com>
2025-01-31 10:04:37 +0000

    Added the random hadith script

---
+++ hadith.js
@@ -0,0 +1,12 @@
+const HadithText = document.querySelector("#HadithOfTheDayText")
+const HadithArray = [
+
+    "Whoever misses Asr (Intentionally) then it is as if he has lost his family and property",
+    "Beware of suspicion for it is the most untruthful type of speech.",
+    "Whoever stands [for night prayer] in Ramadan out of faith and hope for reward will be forgiven his past sins.",
+    "One Umrah to the next is an expiation for whatever happened between them and the only reward for an accepted Hajj is paradise."
+
+]
+
+const Hadith = Math.floor(Math.random() * 4)
+HadithText.textContent = HadithArray[Hadith]
